2f1cfbc59e50c7b001e4839def24ddd0206cc445
Sahil pillania <sahilpillania88@gmail.com>
2024-02-07 21:28:51 +0530

    Added colors var

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -2,17 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MyThemes {
-      static ThemeData lightTheme(BuildContext context) => ThemeData(
-          primarySwatch: Colors.green,
-          fontFamily: GoogleFonts.lato().fontFamily,
-          appBarTheme: AppBarTheme(
-            color: Colors.white,
-            elevation: 0.0,
-            iconTheme: IconThemeData(color: const Color.fromARGB(255, 0, 0, 0)),
-
-          ));
-
-           static ThemeData darkTheme(BuildContext context) => ThemeData(
+  static ThemeData lightTheme(BuildContext context) => ThemeData(
       primarySwatch: Colors.green,
       fontFamily: GoogleFonts.lato().fontFamily,
       appBarTheme: AppBarTheme(
@@ -20,4 +10,17 @@ class MyThemes {
         elevation: 0.0,
         iconTheme: IconThemeData(color: const Color.fromARGB(255, 0, 0, 0)),
       ));
+
+  static ThemeData darkTheme(BuildContext context) => ThemeData(
+      primarySwatch: Colors.green,
+      fontFamily: GoogleFonts.lato().fontFamily,
+      appBarTheme: AppBarTheme(
+        color: Colors.white,
+        elevation: 0.0,
+        iconTheme: IconThemeData(color: const Color.fromARGB(255, 0, 0, 0)),
+      ));
+
+// Colors
+  static Color creamColor = Color(0xfff5f5f5);
+  static Color darkBlueColor = Color(0xff403b58);
 }
