b1a1080a6fb41f2d8380e84dca7244f1ec11a357
Sahil pillania <sahilpillania88@gmail.com>
2024-02-09 20:36:11 +0530

    Added theme properties

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class MyThemes {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
-      primarySwatch: Colors.green,
+      primarySwatch: Colors.deepPurple,
+      cardColor: Colors.white,
+      canvasColor: creamColor,
+      indicatorColor: darkBlueColor,
+      primaryColor: Colors.black,
+      buttonTheme: ButtonThemeData(buttonColor: darkBlueColor),
       fontFamily: GoogleFonts.lato().fontFamily,
       appBarTheme: AppBarTheme(
         color: Colors.white,
@@ -12,15 +18,23 @@ class MyThemes {
       ));
 
   static ThemeData darkTheme(BuildContext context) => ThemeData(
-      primarySwatch: Colors.green,
+      brightness: Brightness.dark,
+      // primarySwatch: Colors.green,
+      indicatorColor: lightBlueColor,
+      primaryColor: Colors.white,
+      cardColor: Colors.black,
+      canvasColor: darkCreamColor,
+      buttonTheme: ButtonThemeData(buttonColor: lightBlueColor),
       fontFamily: GoogleFonts.lato().fontFamily,
       appBarTheme: AppBarTheme(
-        color: Colors.white,
+        color: Colors.black,
         elevation: 0.0,
-        iconTheme: IconThemeData(color: const Color.fromARGB(255, 0, 0, 0)),
+        iconTheme: IconThemeData(color: Colors.white),
       ));
 
 // Colors
   static Color creamColor = Color(0xfff5f5f5);
+  static Color darkCreamColor = Vx.gray900;
   static Color darkBlueColor = Color(0xff403b58);
+  static Color lightBlueColor = Vx.indigo500;
 }
