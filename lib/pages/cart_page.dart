b4ca3905aa4ab61f9310109c6998ec9364055960
Sahil pillania <sahilpillania88@gmail.com>
2024-02-10 20:26:26 +0530

    Cart page UI

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -9,14 +9,65 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: context.canvasColor,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: Text(
-          "Cart",
-          style: TextStyle(color: context.primaryColor),
+        backgroundColor: context.canvasColor,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: Text(
+            "Cart",
+            style: TextStyle(color: context.primaryColor),
+          ),
         ),
-      ),
+        body: Column(
+          children: [
+            _CartList().p32().expand(),
+            Divider(),
+            _CartTotal(),
+          ],
+        ));
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  const _CartTotal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+        "\$9999".text.xl4.color(context.theme.primaryColor).make(),
+        30.widthBox,
+        ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                        context.theme.indicatorColor)),
+                onPressed: () {},
+                child: "Buy".text.xl2.color(Colors.white).make())
+            .w32(context),
+      ]),
     );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  const _CartList({super.key});
+
+  @override
+  State<_CartList> createState() => _CartListState();
+}
+
+class _CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) => ListTile(
+              leading: Icon(Icons.done),
+              trailing: IconButton(
+                icon: Icon(Icons.remove_circle_outline),
+                onPressed: () {},
+              ),
+              title: "Item 1".text.make(),
+            ));
   }
 }
